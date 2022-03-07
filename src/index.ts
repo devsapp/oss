8ac77318e383a535141faa711806aa682ec2de43
wss-git <wssgryx@163.com>
2022-03-07 15:42:44 +0800

    Program error throw error

--- src/index.ts
+++ src/index.ts
@@ -48,8 +48,7 @@ export default class OssComponent {
     const customDomains = get(inputs, 'props.customDomains', {});
     let ossBucket = get(inputs, 'props.bucket');
     if (!ossBucket) {
-      logger.error('bucket is required For oss');
-      return;
+      throw new cores.CatchableError('bucket is required For oss');
     } else if (ossBucket === 'auto') {
       const serviceName = get(inputs, 'project.projectName');
       ossBucket = `serverless-devs-${region}-${serviceName}-${uid}`;
@@ -74,61 +73,54 @@ export default class OssComponent {
     const ossSrc = get(inputs, 'props.codeUri');
     if (!fs.existsSync(ossSrc)) {
       const errString = `no such file or directory, stat '${ossSrc}'`;
-      logger.error(errString);
-      return;
+      throw new cores.CatchableError(errString);
     }
-    try {
-      // update ossAcl
-      await ossClient.putBucketACL(ossBucket, ossAcl);
-      // update ossCors allowedOrigin allowedMethod,  params is required
-      const ossCors = get(inputs, 'props.cors', []);
-      !isEmpty(ossCors) && (await ossClient.putBucketCORS(ossBucket, ossCors));
-      // update ossReferer
-      const ossReferer = get(inputs, 'props.referer', {});
-      const { allowEmpty = true, referers: ossReferers = [] } = ossReferer;
-      await ossClient.putBucketReferer(ossBucket, allowEmpty, ossReferers);
-      // put file ossSrc
-      const ossSubDir = get(inputs, 'props.subDir');
-      await put(ossClient, ossSrc, ossSubDir);
-      // update website
-      const ossStatic: IOssStatic = get(inputs, 'props.website', {});
-      const { index = '', error = '', subDir = '' } = ossStatic;
-      const websiteConfig: IwebsiteConfig = { index, error };
-      if (subDir && subDir.type) {
-        // supportSubDir ?
-        websiteConfig.supportSubDir = true;
-        const typeMap = {
-          noSuchKey: 1,
-          index: 2,
-          redirect: 0,
-        };
-        const subDirType = get(typeMap, subDir.type, 1);
-        websiteConfig.type = subDirType;
-      }
-      await ossClient.putBucketWebsite(ossBucket, websiteConfig);
-      // bindDomain
-      const result: IOssRes = {
-        Bucket: ossBucket,
-        Region: get(inputs, 'props.region'),
+    
+    // update ossAcl
+    await ossClient.putBucketACL(ossBucket, ossAcl);
+    // update ossCors allowedOrigin allowedMethod,  params is required
+    const ossCors = get(inputs, 'props.cors', []);
+    !isEmpty(ossCors) && (await ossClient.putBucketCORS(ossBucket, ossCors));
+    // update ossReferer
+    const ossReferer = get(inputs, 'props.referer', {});
+    const { allowEmpty = true, referers: ossReferers = [] } = ossReferer;
+    await ossClient.putBucketReferer(ossBucket, allowEmpty, ossReferers);
+    // put file ossSrc
+    const ossSubDir = get(inputs, 'props.subDir');
+    await put(ossClient, ossSrc, ossSubDir);
+    // update website
+    const ossStatic: IOssStatic = get(inputs, 'props.website', {});
+    const { index = '', error = '', subDir = '' } = ossStatic;
+    const websiteConfig: IwebsiteConfig = { index, error };
+    if (subDir && subDir.type) {
+      // supportSubDir ?
+      websiteConfig.supportSubDir = true;
+      const typeMap = {
+        noSuchKey: 1,
+        index: 2,
+        redirect: 0,
       };
-      if (isEmpty(customDomains)) {
-        result.OssAddress = `https://oss.console.aliyun.com/bucket/${ossRegion}/${ossBucket}/object`;
-      } else {
-        // 如果auto 修改 bucket
-        const { domains: domainList, reportContent } = await bindDomain(inputs, ossBucket);
-        // report oss response
-        result.Domains = domainList;
-      }
-      const message = every(result.Domains, (child) => isEmpty(child))
-        ? 'oss deployed successful without Domain'
-        : 'oss deployed successful ';
-      logger.info(message);
-      return result;
-    } catch (e) {
-      logger.error('oss deployed aborted');
-      return {
-        errMesg: e,
-      };
+      const subDirType = get(typeMap, subDir.type, 1);
+      websiteConfig.type = subDirType;
     }
+    await ossClient.putBucketWebsite(ossBucket, websiteConfig);
+    // bindDomain
+    const result: IOssRes = {
+      Bucket: ossBucket,
+      Region: get(inputs, 'props.region'),
+    };
+    if (isEmpty(customDomains)) {
+      result.OssAddress = `https://oss.console.aliyun.com/bucket/${ossRegion}/${ossBucket}/object`;
+    } else {
+      // 如果auto 修改 bucket
+      const { domains: domainList, reportContent } = await bindDomain(inputs, ossBucket);
+      // report oss response
+      result.Domains = domainList;
+    }
+    const message = every(result.Domains, (child) => isEmpty(child))
+      ? 'oss deployed successful without Domain'
+      : 'oss deployed successful ';
+    logger.info(message);
+    return result;
   }
 }
